e31ff31fe9d6fad6c41db4894869be54cef4a2f5
ashish <ashish.dutt8@gmail.com>
2017-05-15 17:30:06 +0800

    folder location changed

--- scripts/indian_trains/get_indian_trains_timetable.R
+++ scripts/indian_trains/get_indian_trains_timetable.R
@@ -1,6 +1,11 @@
 # objective: create spatial data for Indian Railways
 # data from https://data.gov.in/catalog/indian-railways-train-time-table-0
 # inspiration http://curiousanalytics.blogspot.com.es/2015/04/indian-railways-network-using-r.html
+# inspiration: https://timogrossenbacher.ch/2016/12/beautiful-thematic-maps-with-ggplot2-only/
+
+# clean the workspace
+rm(list = ls())
+
 # load packages
 library("dplyr")
 library("readr")
@@ -26,6 +31,11 @@ train_timetable <- timetable %>%
   # Remove 'JN', which represents junction, from station names     
   mutate(sourceStationName = gsub(" JN", "", sourceStationName),
          destStationName = gsub(" JN", "", destStationName),
-         stationName = gsub(" JN", "", stationName))
-
+         stationName = gsub(" JN", "", stationName)) %>%
+  # Append "railway India" in station name for acquiring correct geocodes
+  mutate(sourceStationName = paste0(sourceStationName, " railway India"),
+       destStationName = paste0(destStationName, " railway India"),
+       stationName = paste0(stationName, " railway India")) 
+  
+head(train_timetable)
 save(timetable, file = "data/train_timetable.Rdata")

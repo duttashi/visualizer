4a5e835b73644303d20c2d306e34ecc7501fe6fa
Ashish Dutt <ashish.dutt8@gmail.com>
2017-05-16 11:09:28 +0800

    add bivariate density code

--- scripts/pretty_scatterplot.R
+++ scripts/pretty_scatterplot.R
@@ -26,6 +26,7 @@ ggplot(df, aes(var1, var2, color=var1))+
 #  the problem is that the color is changing as the points go from left to right. Instead, we want the color to change in a direction that characterises the correlation – diagonally in this case.
 # To do this, we can color points by the first principal component. Add it to the data frame as a variable pc and use it to color like so:
 df$pc<- predict(princomp(~var1+var2, df))[,1]
+
 ggplot(df, aes(var1, var2, color=pc))+
   geom_point(shape=16, size=5)+
   theme_minimal()
@@ -41,3 +42,13 @@ ggplot(df, aes(var1, var2, color = pc)) +
   theme_minimal() +
   scale_color_gradient(low = "#86e14d", high = "#ee531f")+
   scale_alpha(range = c(.05, .25))
+# Notice, there are some extreme points alone in the space. To resolve this, we use bivariate density which can be added as follows
+# Add bivariate density for each point
+df$density <- fields::interp.surface(MASS::kde2d(df$var1, df$var2), df[,c("var1", "var2")])
+
+ggplot(df, aes(var1, var2, color = pc, alpha = 1/density)) +
+  geom_point(shape = 16, size = 5, show.legend = FALSE,
+             alpha = .4) +
+  theme_minimal() +
+  scale_color_gradient(low = "#86e14d", high = "#ee531f")+
+  scale_alpha(range = c(.25, .6))

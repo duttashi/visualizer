673a93c40fd44f8d6e4474afb06d09b9e4a36346
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-05 21:45:13 +0800

    initial commit

---
+++ scripts/my_custom_theme_01.R
@@ -0,0 +1,45 @@
+# Script Objective: To create a custom theme for data plotting
+
+# Create dummy dataset
+y1 <- round(rnorm(n = 36, mean = 7, sd = 2)) # Simulate data from normal distribution
+y2 <- round(rnorm(n = 36, mean = 21, sd = 6))
+y3 <- round(rnorm(n = 36, mean = 50, sd = 8))
+x <- rep(LETTERS[1:12], 3)
+grp <- rep(c("Grp 1", "Grp 2", "Grp 3"), each = 12)
+dat <- data.frame(grp, x, y1, y2, y3)
+str(dat)
+
+library(ggplot2)
+# Using default theme
+p1 <- ggplot(data = dat, aes(x = x, y = y1)) + geom_bar(stat = "identity", fill = "#552683") +
+  coord_flip() + ylab("Y LABEL") + xlab("X LABEL") + facet_grid(. ~ grp) +
+  ggtitle("TITLE OF THE FIGURE")
+p1
+
+# So that's the default theme of ggplot2, and we want to customize this using the theme function. 
+# Step 1: I want to tweak the font first;
+library(extrafont)
+#font_import() # Import all fonts
+fonts() # Print list of all fonts
+
+# Configure Theme
+my_theme01 <- function() {
+  theme(
+    plot.background = element_rect(fill = "#E2E2E3", colour = "#E2E2E3"),
+    panel.background = element_rect(fill = "#E2E2E3"),
+    panel.background = element_rect(fill = "white"),
+    axis.text = element_text(colour = "#E7A922", family = "Impact"),
+    plot.title = element_text(colour = "#552683", face = "bold", size = 18, vjust = 1, family = "Impact"),
+    axis.title = element_text(colour = "#552683", face = "bold", size = 13, family = "Impact"),
+    panel.grid.major.x = element_line(colour = "#E7A922"),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    strip.text = element_text(family = "Impact", colour = "white"),
+    strip.background = element_rect(fill = "#E7A922"),
+    axis.ticks = element_line(colour = "#E7A922")
+  )
+}
+
+# run the theme
+p1+my_theme01()

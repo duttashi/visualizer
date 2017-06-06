2ae5d9235592fad76c7ff79d474901f46b580403
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-06 10:42:02 +0800

    updated the custom theme added new code for scatter plot example AND box plot. Tested with custom theme. working Ok.

--- scripts/my_custom_theme_01.R
+++ scripts/my_custom_theme_01.R
@@ -1,45 +1,63 @@
 # Script Objective: To create a custom theme for data plotting
 
-# Create dummy dataset
-y1 <- round(rnorm(n = 36, mean = 7, sd = 2)) # Simulate data from normal distribution
-y2 <- round(rnorm(n = 36, mean = 21, sd = 6))
-y3 <- round(rnorm(n = 36, mean = 50, sd = 8))
-x <- rep(LETTERS[1:12], 3)
-grp <- rep(c("Grp 1", "Grp 2", "Grp 3"), each = 12)
-dat <- data.frame(grp, x, y1, y2, y3)
-str(dat)
-
-library(ggplot2)
-# Using default theme
-p1 <- ggplot(data = dat, aes(x = x, y = y1)) + geom_bar(stat = "identity", fill = "#552683") +
-  coord_flip() + ylab("Y LABEL") + xlab("X LABEL") + facet_grid(. ~ grp) +
-  ggtitle("TITLE OF THE FIGURE")
-p1
-
-# So that's the default theme of ggplot2, and we want to customize this using the theme function. 
 # Step 1: I want to tweak the font first;
 library(extrafont)
 #font_import() # Import all fonts
 fonts() # Print list of all fonts
 
-# Configure Theme
+# Step 2: Creating a custom theme using the theme()
 my_theme01 <- function() {
   theme(
     plot.background = element_rect(fill = "#E2E2E3"),
     #panel.background = element_rect(fill = "#E2E2E3"),
     panel.background = element_rect(fill = "white"),
-    axis.text = element_text(colour = "#E7A922", family = "Impact"),
-    plot.title = element_text(colour = "#552683", face = "bold", size = 18, vjust = 1, family = "Impact"),
-    axis.title = element_text(colour = "#552683", face = "bold", size = 13, family = "Impact"),
+    axis.text = element_text(colour = "black", family = "Garamond"),
+    plot.title = element_text(colour = "#552683", face = "bold", size = 18, vjust = 1, family = "Garamond"),
+    axis.title = element_text(colour = "#552683", face = "bold", size = 13, family = "Garamond"),
     panel.grid.major.x = element_line(colour = "#E7A922"),
-    panel.grid.minor.x = element_blank(),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    strip.text = element_text(family = "Impact", colour = "white"),
-    strip.background = element_rect(fill = "#E7A922"),
+    #panel.grid.minor.x = element_blank(),
+    #strip.text = element_text(family = "Garamond", colour = "white"),
+    #strip.background = element_rect(fill = "#E7A922"),
     axis.ticks = element_line(colour = "#E7A922")
   )
 }
 
+
+# Testing the custom theme
+
+library(ggplot2)
+
+# Using default theme
+# Note: bar graphs are typically used to display numeric values (on the y-axis), for different categories (on the x-axis)
+p1 <- ggplot(data = diamonds, aes(x = cut, y = carat)) + 
+  geom_bar(stat = "identity", fill = "#552683") +
+  coord_flip() + ylab("Y LABEL") + xlab("X LABEL") +
+  ggtitle("TITLE OF THE FIGURE")
+p1
+
 # run the theme
 p1+my_theme01()
+
+# A Scatter plot
+p2<- ggplot(data = diamonds, aes(x=carat, y=z))+
+  geom_point(shape=21)+    # Use hollow circles
+  geom_smooth(method=lm)+  # Add linear regression line 
+                          #  (by default includes 95% confidence region)
+  ylab("Y LABEL") + xlab("X LABEL") +
+  ggtitle("TITLE OF THE FIGURE")
+p2
+# test the theme
+p2+my_theme01()
+
+# A box plot
+?geom_boxplot
+p3<- ggplot(data = diamonds, aes(x=cut, y=price))+
+  geom_boxplot(outlier.colour = "red")+
+  ggtitle("TITLE OF THE FIGURE")
+  
+p3
+# test the theme
+p3+my_theme01()
+
+
+

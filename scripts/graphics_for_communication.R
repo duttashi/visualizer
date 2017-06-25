6bb8c2aa9977704ee37ab8f2ab4ad6329b661c1f
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-25 18:08:50 +0800

    additional code added for plotting

--- scripts/graphics_for_communication.R
+++ scripts/graphics_for_communication.R
@@ -2,6 +2,9 @@
 # Objective: Use visuals to tell the story
 # Data analysis domain: Exploratry Data analysis
 
+# Libraries required
+library(tidyverse)
+
 # Note: To add labels to a plot, use the labs() function.
 ggplot(mpg, aes(displ, hwy)) +
   geom_point(aes(color = class)) +
@@ -27,3 +30,74 @@ ggplot(mpg, aes(displ, hwy)) +
     y = "Highway fuel economy (mpg)",
     colour = "Car type"
   )
+
+# Note: adding labels to data points such that do not overlap. Use the geom_label_repel of the ggrepel() library. An example is given below
+best_in_class <- mpg %>%
+  group_by(class) %>%
+  filter(row_number(desc(hwy)) == 1)
+
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(colour = class)) +
+  geom_point(size = 3, shape = 1, data = best_in_class) +
+  ggrepel::geom_label_repel(aes(label = model), data = best_in_class)
+
+# Note:  To make your plot better for communication is to adjust the scales. Scales control the mapping from data values to things that you can perceive. Normally, ggplot2 automatically adds scales for you. 
+
+# Note: There are two primary arguments that affect the appearance of the ticks on the axes and the keys on the legend: breaks and labels. Breaks controls the position of the ticks, or the values associated with the keys. Labels controls the text label associated with each tick/key. The most common use of breaks is to override the default choice:
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point() +
+  scale_y_continuous(breaks = seq(15, 40, by = 5))
+# note: Collectively axes and legends are called guides. Axes are used for x and y aesthetics; legends are used for everything else.
+
+# Note: Legend Layout- To control the overall position of the legend, you need to use a theme() setting. The theme setting legend.position controls where the legend is drawn:
+base <- ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(colour = class))
+
+base + theme(legend.position = "left")
+base + theme(legend.position = "top")
+base + theme(legend.position = "bottom")
+base + theme(legend.position = "right") # the default
+
+# note: To zoom in on a region of the plot, it’s generally best to use coord_cartesian(). Compare the following two plots:
+ggplot(mpg, mapping = aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth() +
+  coord_cartesian(xlim = c(5, 7), ylim = c(10, 30))
+
+mpg %>%
+  filter(displ >= 5, displ <= 7, hwy >= 10, hwy <= 30) %>%
+  ggplot(aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth()
+#note:  Reducing the limits is basically equivalent to subsetting the data. It is generally more useful if you want expand the limits, for example, to match scales across different plots. For example, if we extract two classes of cars and plot them separately, it’s difficult to compare the plots because all three scales (the x-axis, the y-axis, and the colour aesthetic) have different ranges.
+suv <- mpg %>% filter(class == "suv")
+compact <- mpg %>% filter(class == "compact")
+
+# the range for variable hwy is between 12-24
+ggplot(suv, aes(displ, hwy, colour = drv)) +
+  geom_point() 
+# the range for variable hwy is between 25-45
+ggplot(compact, aes(displ, hwy, colour = drv)) +
+  geom_point()
+# One way to overcome this problem is to share scales across multiple plots, training the scales with the limits of the full data.
+x_scale <- scale_x_continuous(limits = range(mpg$displ))
+y_scale <- scale_y_continuous(limits = range(mpg$hwy))
+col_scale <- scale_colour_discrete(limits = unique(mpg$drv))
+
+ggplot(suv, aes(displ, hwy, colour = drv)) +
+  geom_point() +
+  x_scale +
+  y_scale +
+  col_scale
+
+ggplot(compact, aes(displ, hwy, colour = drv)) +
+  geom_point() +
+  x_scale +
+  y_scale +
+  col_scale
+
+# Note: Themes: Finally, you can customise the non-data elements of your plot with a theme:
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  theme_bw()

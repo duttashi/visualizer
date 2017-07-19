e54d3753277383535bc9a061843d44f032018209
Ashish Dutt <ashish.dutt8@gmail.com>
2017-07-19 17:49:00 +0800

    initial commit

---
+++ scripts/misc_lattice_example.R
@@ -0,0 +1,14 @@
+# Lattice Examples 
+library(lattice) 
+attach(mtcars)
+
+# create factors with value labels 
+gear.f<-factor(gear,levels=c(3,4,5),
+               labels=c("3gears","4gears","5gears")) 
+cyl.f <-factor(cyl,levels=c(4,6,8),
+               labels=c("4cyl","6cyl","8cyl")) 
+
+# kernel density plot 
+densityplot(~mpg, 
+            main="Density Plot", 
+            xlab="Miles per Gallon")

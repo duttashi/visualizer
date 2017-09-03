7f975adb59eb8ac35854932283fcfc31b988659d
Ashish Dutt <ashish.dutt8@gmail.com>
2017-09-03 13:40:20 +0800

    issue resolved.

--- scripts/quick_recipes/how_to_saving_ggplot_graph_to_PDF_with_fonts_embedded.R
+++ scripts/quick_recipes/how_to_saving_ggplot_graph_to_PDF_with_fonts_embedded.R
@@ -1,3 +1,5 @@
+# See this github issue https://github.com/duttashi/visualize/issues/18
+
 library(ggplot2)
 library(extrafont)
 
@@ -6,27 +8,30 @@ windowsFonts()
 # tell where ghostscript is located. This is required for saving the font in pdf
 Sys.setenv(R_GSCMD = "C:\\Program Files\\gs\\gs9.21\\bin\\gswin64c.exe")
 
-# create a plot object
+# create a plot object and change the font to Calibri
 p <- ggplot(mtcars, aes(x=wt, y=mpg)) + 
   geom_point()+
   ggtitle("Fuel Efficiency of 32 Cars")+
   xlab("Weight (x1000 lb)") + ylab("Miles per Gallon") +
   theme_bw()+
-  theme(text=element_text(family = "Calibri"),
-        axis.text=element_text(size=10), axis.title=element_text(size=15),
-        plot.title = element_text(hjust=0.5, size=20))
-
-# save the plot as pdf  
+  theme(text=element_text(family = "Calibri"), axis.text=element_text(size=10), 
+        axis.title=element_text(size=15),
+        plot.title = element_text(hjust=0.5, size=20)
+  )
 print(p)
-ggsave("figures//ggplot_calibri.pdf", p, width=10, height=10)
-embed_fonts("figures//ggplot_calibri.pdf", outfile="figures//ggplot_calibri_embed.pdf")
 
-# If overlapping then first check the current graphic device using `dev.cur()`. 
-# This will give the number for current graphic device. Use that number in dev.off to turn off that graphic device
-# null device 1 means that there are no active graphics devices available. 
-
-pie(10)
-dev.cur()
-dev.off(2)
-
-
+# Use font ArialMT because only this font will work with ggsave()
+q <- ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point()+
+  ggtitle("Fuel Efficiency of 32 Cars")+
+  xlab("Weight (x1000 lb)") + ylab("Miles per Gallon") +
+  theme_bw()+
+  theme(text=element_text(family = "ArialMT"), axis.text=element_text(size=10), 
+        axis.title=element_text(size=15),
+        plot.title = element_text(hjust=0.5, size=20)
+  )
+# show the plot
+print(q)
+# save the plot as pdf
+ggsave("figures//ggplot_arialmt.pdf", q, width=20, height=20, 
+       device = "pdf", units = "cm")

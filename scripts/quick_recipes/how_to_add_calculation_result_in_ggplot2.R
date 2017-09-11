dcd310277d2d2b7c6e0a09ce735d8f8ca6460e02
duttashi <ashish.dutt8@gmail.com>
2017-09-11 17:13:58 +0800

    added method 2 as another alternative

--- scripts/quick_recipes/how_to_add_calculation_result_in_ggplot2.R
+++ scripts/quick_recipes/how_to_add_calculation_result_in_ggplot2.R
@@ -8,6 +8,7 @@ library(dplyr)
 
 result <- mtcars
 
+# Method 1: The problem is text is bolded as it overlays on itself
 result %>%
   group_by(gear) %>%
   mutate(count = n(), sum_wt = sum(wt)) %>%
@@ -24,3 +25,23 @@ result %>%
   geom_text(aes(label = paste0("sum wt = ", sum_wt), 
                 x = (gear - 0.25),
                 y = 4.75)) 
+
+# Method 2: Alternatively, if you create a summary data frame named result_sum, then you can manually add that into the geom_text calls.
+result_sum <- result %>%
+  group_by(gear) %>%
+  summarise(count = n(), sum_wt = sum(wt))
+
+
+ggplot(result, aes(x = gear, y = drat, , group=gear)) +
+  theme_tufte(base_size = 15) + theme(line=element_blank()) +
+  geom_violin(fill = "white") +
+  geom_boxplot(fill = "black", alpha = 0.3, width = 0.1) +
+  ylab("drat") + 
+  xlab("gear") +
+  coord_flip()+
+  geom_text(data = result_sum, aes(label = paste0("n = ", count), 
+                                   x = (gear + 0.25), 
+                                   y = 4.75)) +
+  geom_text(data = result_sum, aes(label = paste0("sum wt = ", sum_wt), 
+                                   x = (gear - 0.25),
+                                   y = 4.75))

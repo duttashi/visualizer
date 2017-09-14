7d536bad93530061a505f86109f3a392a0002afc
duttashi <ashish.dutt8@gmail.com>
2017-09-14 16:07:26 +0800

    initial commit

---
+++ scripts/uncategorized/book-1.R
@@ -0,0 +1,22 @@
+# excerpts from the book: Data Visualization for Social Science A practical introduction with R and ggplot2 by Kieran Healy accessible at http://socviz.co/
+
+# required packages
+my_packages <- c("tidyverse", "broom", "coefplot", "cowplot",
+                 "gapminder", "GGally", "ggjoy", "ggrepel", "gridExtra",
+                 "interplot", "margins", "maps", "mapproj", "mapdata",
+                 "MASS", "quantreg", "scales", "survey", "srvyr",
+                 "viridis", "viridisLite", "devtools")
+
+# install the required packages if not already installed
+install.packages(my_packages, repos = "http://cran.rstudio.com")
+
+#devtools::install_github("kjhealy/socviz")
+
+# Load the libraries
+library(ggplot2)
+ggplot(data = mpg, aes(x=displ, y=hwy))+
+  geom_point()
+library(gapminder)
+head(gapminder)
+p<-ggplot(data = gapminder, aes(x=gdpPercap, y=lifeExp))
+p+geom_point()

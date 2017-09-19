55c45851235ad1f421777502a4e68bcfc245417d
duttashi <ashish.dutt8@gmail.com>
2017-09-19 15:25:56 +0800

    initial commit

---
+++ scripts/uncategorized/order_bars_in_ggplot2.R
@@ -0,0 +1,6 @@
+# reference: https://stackoverflow.com/questions/5208679/order-bars-in-ggplot2-bar-graph?rq=1
+# question: to make a bar graph where the largest bar would be nearest to the y axis and the shortest bar would be furthest. 
+
+library(ggplot2) 
+library(reshape2) # to convert to long format 
+

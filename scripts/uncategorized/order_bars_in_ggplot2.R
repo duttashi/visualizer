f9b1b1178f46356071866c6adc3f4588a7a717e3
duttashi <ashish.dutt8@gmail.com>
2017-09-25 17:15:30 +0800

    initial commit

--- scripts/uncategorized/order_bars_in_ggplot2.R
+++ scripts/uncategorized/order_bars_in_ggplot2.R
@@ -1,6 +1,17 @@
-# reference: https://stackoverflow.com/questions/5208679/order-bars-in-ggplot2-bar-graph?rq=1
+# reference: https://stackoverflow.com/questions/16961921/plot-data-in-descending-order-as-appears-in-data-frame?noredirect=1&lq=1
 # question: to make a bar graph where the largest bar would be nearest to the y axis and the shortest bar would be furthest. 
 
 library(ggplot2) 
 library(reshape2) # to convert to long format 
 
+set.seed(42)
+df <- data.frame(Category = sample(LETTERS), Count = rpois(26, 6))
+
+p1 <- ggplot(df, aes(x = Category, y = Count)) +
+  geom_bar(stat = "identity")
+
+p2 <- ggplot(df, aes(x = reorder(Category, -Count), y = Count)) +
+  geom_bar(stat = "identity")
+
+require("gridExtra")
+grid.arrange(arrangeGrob(p1, p2))
